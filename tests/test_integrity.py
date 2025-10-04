ba3d118e6943057c6c0368865fb47e0ae4a7bb47
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-04 14:15:31 +0100

    add test_hash_computation function

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -2,3 +2,14 @@ import os
 import tempfile
 from integrity_check import compute_hash, init, check, update, load_hashes
 from integrity_checker.integrity_check import compute_hash, init, check, update, load_hashes
+
+def test_hash_computation():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(b"hello world")
+        tmp_path = tmp.name
+    hash1 = compute_hash(tmp_path)
+    with open(tmp_path, "a") as f:
+        f.write("!")
+    hash2 = compute_hash(tmp_path)
+    os.remove(tmp_path)
+    assert hash1 != hash2
