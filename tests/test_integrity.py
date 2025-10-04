c6a4a1a84c0ac9050365f1e9dae0a31a9ee5d29b
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-04 14:15:13 +0100

    change integrity_check import settings

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -1,3 +1,4 @@
 import os
 import tempfile
 from integrity_check import compute_hash, init, check, update, load_hashes
+from integrity_checker.integrity_check import compute_hash, init, check, update, load_hashes
