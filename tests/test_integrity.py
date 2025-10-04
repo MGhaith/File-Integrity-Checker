be432f59f1c22219367778da09394f21b97730ff
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-04 14:15:53 +0100

    Update test_integrity.py

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -1,6 +1,5 @@
 import os
 import tempfile
-from integrity_check import compute_hash, init, check, update, load_hashes
 from integrity_checker.integrity_check import compute_hash, init, check, update, load_hashes
 
 def test_hash_computation():
